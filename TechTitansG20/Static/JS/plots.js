d07684b5fa4df371c487b55ccef1a7347152712d
John Anh Nguyen <John.nguyen.TA@gmail.com>
2024-02-26 21:13:52 -0500

    import draft chart and data

--- TechTitansG20/Static/JS/plots.js
+++ TechTitansG20/Static/JS/plots.js
@@ -3,27 +3,32 @@ d3.json(url).then(function(response) {
     console.log(response);
 
 
-let trace 1 = {
-    x: ,
-    y: ,
-    text: ,
-    name: ,
-    type: 'bar'
+let trace1 = {
+        x: Object.keys(response),  // Assuming the response is an object with categories as keys
+        y: Object.values(response), // Assuming the response values are the data points
+        text: 'blabla',
+        name: 'funny chart',
+        type: 'bar'
 };
-
-let trace 2 = {
-    x: ,
-    y: ,
-    text: ,
-    name: ,
-    type: 'bar'
-};
+// let trace2 = {
+//     x: [32,64,78,34,54,90],
+//     y: [90,102,127,8,29],
+//     text: 'cool',
+//     name: 'next chart',
+//     type: 'bar'
+// };
 
 // create data array
 // create data array
-let data = [trace1, trace2];
+let data = [trace1];
 let layout = {
-    title: "xxxxxxxxx",
+    title: "Bar Chart",
+    xaxis: {
+        title: 'Categories'
+    },
+    yaxis: {
+        title: 'Values'
+    },
     barmode: "group",
     margin: {
         l: 50,
@@ -35,5 +40,5 @@ let layout = {
 };
 
 // plot the data
-plotly.newPlot("plot", data, layout);
+Plotly.newPlot("plot", data, layout);
 });
--- TechTitansG20/app.py
+++ TechTitansG20/app.py
@@ -5,21 +5,29 @@ from sqlalchemy import create_engine, text, inspect, func
 from flask import Flask, jsonify, render_template
 
 # Create the engine
-engine = create_engine("sql:///GlobalTechDB.sql", echo = False)
+# engine = create_engine("sql:///GlobalTechDB.sql", echo = False)
+#insert fake data
+
+
 
 # Reflect the database schema
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-GlobalTechDB = Base.classes.GlobalTechDB
-
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
+# GlobalTechDB = Base.classes.GlobalTechDB
 # Create the Flask application
 app = Flask (__name__)
-
+data = {
+        "category1": 50,
+        "category2": 75,
+        "category3": 60,
+        "category4": 80,
+        "category5": 90
+    }
 
 @app.route("/api/v1.0/global_data")
 def global_data():
-    session = session (engine)
-
+    # session = session (engine)
+    return jsonify(data)
 
 
 @app.route("/")
